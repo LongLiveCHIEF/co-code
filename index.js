1f2c11402a0d689d3254a393cf1845117a2c8b84
Paul Kimbrel <paul@paulkimbrel.com>
2016-06-15 11:24:03 -0500

    Added code mirror

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var serveStatic = require('serve-static');
+
+app.use("/vendor", serveStatic("node_modules"));
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+var fileContents = "This is a file\nWe should change it";
+
 
 app.get("/", function (req, res) {
     res.sendfile('public/index.html');
@@ -14,7 +22,17 @@ io.on('connection', function(socket) {
     socket.on('chat message', function(msg) {
         io.emit('chat message', msg);
     });
+    
+    socket.on('incoming change', function(msg) {
+        console.log(msg);
+        // Update the file here
+        //io.emit('outgoing change', fileContents);
+    });
 });
+
+setInterval(function() {
+    io.emit('outgoing change', fileContents);
+}, 3000);
 
 http.listen(8080, function() {
     console.log("On 8080...");
